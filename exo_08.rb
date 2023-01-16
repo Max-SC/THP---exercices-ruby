914ae63c26c500262c9d510a4b85e9f5c3204e28
Maxime SILVA CRUZ <max.scruz.msc@gmail.com>
2023-01-16 20:46:22 +0100

    exercices 6 à 9

---
+++ exo_08.rb
@@ -0,0 +1,4 @@
+puts "Comment vous appelez-vous?"
+print ">" 
+user_name = gets.chomp
+puts "Bonjour " +  user_name
---
+++ exo_09.rb
@@ -0,0 +1,8 @@
+puts "Veuillez entre votre prénom:"
+print ">"
+user_first_name = gets.chomp
+puts "Maintenant veuillez entrer votre nom de famille:"
+print ">"
+user_last_name = gets.chomp
+puts "Merci, chargement en cours ... "
+puts "Bonjour, " + user_first_name + " " + user_last_name
